fd80795d9fe416985c367bbec1b599e4556de41a
Ashwin Hegde <ashwin.hegde3@gmail.com>
2021-06-20 11:37:19 +0530

    feat(example): graph

--- code/graphs/__tests__/graph.specs.js
+++ code/graphs/__tests__/graph.specs.js
@@ -97,6 +97,8 @@ describe('Graph', () => {
     graph.addEdge('Singapore', 'Combodia')
     graph.addEdge('Vietnam', 'Combodia')
 
+    expect(graph.removeVertex('Russia')).toBeFalsy()
+
     initVertex = graph.adjacencyList['India']
     expect(initVertex.length).toBe(3)
     graph.removeVertex('India')
@@ -104,5 +106,77 @@ describe('Graph', () => {
     expect(graph.adjacencyList['Malaysia']).toEqual(expect.not.arrayContaining(['India']))
     expect(graph.adjacencyList['Singapore']).toEqual(expect.not.arrayContaining(['India']))
     expect(graph.adjacencyList['Thailand']).toEqual(expect.not.arrayContaining(['India']))
+  })
+
+  test('should perform depth first search recursively in graph', () => {
+    let graph = new Graph()
+    
+    graph.addVertex('India')
+    graph.addVertex('Malaysia')
+    graph.addVertex('Singapore')
+    graph.addVertex('Thailand')
+    graph.addVertex('Vietnam')
+    graph.addVertex('Combodia')
+    
+    graph.addEdge('India', 'Malaysia')
+    graph.addEdge('India', 'Singapore')
+    graph.addEdge('India', 'Thailand')
+    graph.addEdge('Malaysia', 'Vietnam')
+    graph.addEdge('Singapore', 'Vietnam')
+    graph.addEdge('Malaysia', 'Combodia')
+    graph.addEdge('Singapore', 'Combodia')
+    graph.addEdge('Vietnam', 'Combodia')
+
+    expect(graph.depthFirstSearchRecursive("India").length).toBe(6)
+    expect(graph.depthFirstSearchRecursive("India")).toEqual(expect.arrayContaining(['India', 'Malaysia', 'Vietnam', 'Singapore', 'Combodia', 'Thailand']))
+    expect(graph.depthFirstSearchRecursive("Malaysia")).toEqual(expect.arrayContaining(['Malaysia', 'India', 'Vietnam', 'Singapore', 'Combodia', 'Thailand']))
+  })
+
+  test('should perform depth first search iteratively in graph', () => {
+    let graph = new Graph()
+    
+    graph.addVertex('India')
+    graph.addVertex('Malaysia')
+    graph.addVertex('Singapore')
+    graph.addVertex('Thailand')
+    graph.addVertex('Vietnam')
+    graph.addVertex('Combodia')
+    
+    graph.addEdge('India', 'Malaysia')
+    graph.addEdge('India', 'Singapore')
+    graph.addEdge('India', 'Thailand')
+    graph.addEdge('Malaysia', 'Vietnam')
+    graph.addEdge('Singapore', 'Vietnam')
+    graph.addEdge('Malaysia', 'Combodia')
+    graph.addEdge('Singapore', 'Combodia')
+    graph.addEdge('Vietnam', 'Combodia')
+
+    expect(graph.depthFirstSearchIterative("India").length).toBe(6)
+    expect(graph.depthFirstSearchIterative("India")).toEqual(expect.arrayContaining(['India', 'Malaysia', 'Vietnam', 'Singapore', 'Combodia', 'Thailand']))
+    expect(graph.depthFirstSearchIterative("Malaysia")).toEqual(expect.arrayContaining(['Malaysia', 'India', 'Vietnam', 'Singapore', 'Combodia', 'Thailand']))
+  })
+
+  test('should perform breath first search in graph', () => {
+    let graph = new Graph()
+    
+    graph.addVertex('India')
+    graph.addVertex('Malaysia')
+    graph.addVertex('Singapore')
+    graph.addVertex('Thailand')
+    graph.addVertex('Vietnam')
+    graph.addVertex('Combodia')
+    
+    graph.addEdge('India', 'Malaysia')
+    graph.addEdge('India', 'Singapore')
+    graph.addEdge('India', 'Thailand')
+    graph.addEdge('Malaysia', 'Vietnam')
+    graph.addEdge('Singapore', 'Vietnam')
+    graph.addEdge('Malaysia', 'Combodia')
+    graph.addEdge('Singapore', 'Combodia')
+    graph.addEdge('Vietnam', 'Combodia')
+
+    expect(graph.breadthFirstSearch("India").length).toBe(7)
+    expect(graph.breadthFirstSearch("India")).toEqual(expect.arrayContaining(['India', 'Malaysia', 'Vietnam', 'Singapore', 'Combodia', 'Thailand']))
+    expect(graph.breadthFirstSearch("Malaysia")).toEqual(expect.arrayContaining(['Malaysia', 'India', 'Vietnam', 'Singapore', 'Combodia', 'Thailand']))
   })
 })
--- code/graphs/graph.js
+++ code/graphs/graph.js
@@ -25,11 +25,83 @@ class Graph {
   }
 
   removeVertex(vertex) {
+    if(!this.adjacencyList[vertex]) {
+      return false
+    }
+
     while(this.adjacencyList[vertex].length) {
       const adjacencyVertex = this.adjacencyList[vertex].pop()
       this.removeEdge(vertex, adjacencyVertex)
     }
     delete this.adjacencyList[vertex]
+  }
+
+  depthFirstSearchRecursive(startVertex) {
+    const result = []
+    const visited = {}
+    const adjacencyList = this.adjacencyList; // Semi-colon is request to make engine identify adjacencyList is object not function
+
+    (function dfs(vertex) {
+      if (!vertex) {
+        return null
+      }
+
+      visited[vertex] = true
+      result.push(vertex)
+
+      adjacencyList[vertex].forEach(element => {
+        if (!visited[element]) {
+          return dfs(element)
+        }
+      })
+
+    })(startVertex)
+
+    return result
+  }
+
+  depthFirstSearchIterative(startVertex) {
+    const result = []
+    const visited = {}
+    const stack = [startVertex]
+    let currentVertex;
+
+    visited[startVertex] = true
+
+    while(stack.length) {
+      currentVertex = stack.pop()
+      result.push(currentVertex)
+
+      this.adjacencyList[currentVertex].forEach(element => {
+        if (!visited[element]) {
+          visited[element] = true
+          stack.push(element)
+        } 
+      })
+    }
+
+    return result
+  }
+  
+  breadthFirstSearch(startVertex) {
+    const result = []
+    const visited = {}
+    const queue = [startVertex]
+    let currentVertex;
+
+    while(queue.length) {
+      currentVertex = queue.shift()
+      result.push(currentVertex)
+
+      this.adjacencyList[currentVertex].forEach(element => {
+        if (!visited[element]) {
+          visited[element] = true
+          queue.push(element)
+        }
+      })
+    }
+
+    return result
   }
 }
 
